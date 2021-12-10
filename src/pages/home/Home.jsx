8fb20dc4453a8e6ff97f0d33a41f91c4e0aa5ce6
Dione Martins <profissionaldione@gmail.com>
2021-12-09 21:35:23 -0300

    Created navbar, router and pages. Due to mobile, replaced all vh with %

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Header, BookCard } from '../../components';
+import { Header, Navbar, BookCard } from '../../components';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Navbar />
       <BookCard />
     </div>
   );
