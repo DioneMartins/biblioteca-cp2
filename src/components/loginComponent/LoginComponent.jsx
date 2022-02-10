2f0aeb21aea4b1a5d1ad15f24ae5d2048e52757d
Dione Martins <profissionaldione@gmail.com>
2022-02-10 18:10:18 -0300

    Completed login page

--- src/components/loginComponent/LoginComponent.jsx
+++ src/components/loginComponent/LoginComponent.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import styles from './LoginComponent.module.css';
+
+const {
+  loginWrapper,
+  loginMessage,
+  loginEmailInput,
+  loginPasswordInput,
+  loginButton,
+  loginWarning,
+} = styles;
 
 export default function LoginComponent() {
   const [userEmail, setUserEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [debugLoggedMessage, setDebug] = useState('Não logado');
+  const [errorMessage, setErrorMessage] = useState('');
 
   function handleLoginRequest() {
     const auth = getAuth();
@@ -12,22 +22,56 @@ export default function LoginComponent() {
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        setDebug('Logado');
-        console.log(user);
-        // ...
       })
       .catch((error) => {
         const errorCode = error.code;
-        const errorMessage = error.message;
+        pickErrorMessage(errorCode);
       });
   }
 
+  function pickErrorMessage(codedError) {
+    switch (codedError) {
+      default:
+        setErrorMessage(codedError);
+        break;
+      case 'auth/invalid-email':
+        setErrorMessage('Email inválido');
+        break;
+      case 'auth/internal-error':
+        setErrorMessage('Insira a senha');
+        break;
+      case 'auth/user-not-found':
+        setErrorMessage('Usuário não encontrado');
+        break;
+      case 'auth/wrong-password':
+        setErrorMessage('Senha incorreta');
+        break;
+    }
+  }
+
   return (
-    <>
-      <input type="text" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={(e) => handleLoginRequest()}>Login</button>
-      <p>{debugLoggedMessage}</p>
-    </>
+    <div className={loginWrapper}>
+      <p className={loginMessage}>O login só está disponível para administradores</p>
+      <label htmlFor="user">Usuário:</label>
+      <input
+        id="user"
+        className={loginEmailInput}
+        type="text"
+        value={userEmail}
+        onChange={(e) => setUserEmail(e.target.value)}
+      />
+      <label htmlFor="pass">Senha:</label>
+      <input
+        id="pass"
+        className={loginPasswordInput}
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className={loginButton} onClick={(e) => handleLoginRequest()}>
+        Login
+      </button>
+      <p className={loginWarning}>{errorMessage}</p>
+    </div>
   );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import { Navbar, LoginComponent, Unfinished } from '../../components';
+import { Navbar, LoginComponent } from '../../components';
 
 export default function Login() {
   return (
     <div>
       <Navbar />
       <LoginComponent />
-      <Unfinished
-        pageName="Futuro componente de login."
-        message={['Login só está disponível para administradores']}
-        respDev="Argo"
-        devOrder="In Development"
-      />
     </div>
   );
 }
