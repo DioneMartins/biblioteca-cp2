f462fd6fff8aab41166a05f514cd6854ac63941f
Dione Martins <profissionaldione@gmail.com>
2022-03-02 15:46:21 -0300

    Set check to see if there is user

--- src/components/bookCards/bookCardItem/BookCardItem.jsx
+++ src/components/bookCards/bookCardItem/BookCardItem.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './BookCardItem.module.css';
 import { deleteBook } from '../../../api/api';
+import { checkIfUserExists } from '../../../api/login';
 
 const { cardWrapper, cardTitle, cardAuthor } = styles;
 
 export default function BookCardItem(props) {
+  const isLogged = checkIfUserExists();
+
   // eslint-disable-next-line no-unused-vars
   const { firstName, lastName, notes, quant, title, bookNumber, docID } = props;
   return (
@@ -14,7 +17,7 @@ export default function BookCardItem(props) {
       <p className={cardAuthor}>
         {firstName} {lastName}
       </p>
-      <button onClick={() => deleteBook(docID)}></button>
+      {isLogged ? <button onClick={() => deleteBook(docID)}></button> : null}
     </div>
   );
 }
