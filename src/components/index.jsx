9997f77a6e46848286c8234c763a2da90fe6d665
Dione Martins <profissionaldione@gmail.com>
2022-03-03 00:25:56 -0300

    Finish merge

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,9 +6,6 @@ export { default as Unfinished } from './debugWorkInProgress/DebugWorkInProgress
 export { default as HomeCards } from './homeCards/HomeCards';
 export { default as LoginComponent } from './loginComponent/LoginComponent';
 export { default as Logged } from './logged/Logged';
-<<<<<<< HEAD
 export { default as PagFooter } from './paginationFooter/PaginationFooter';
 export { default as PagHeader } from './paginationHeader/PaginationHeader';
-=======
 export { default as Footer } from './footer/Footer';
->>>>>>> master
